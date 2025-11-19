d794e5c5fdf8d6ee6f5bd6f6fc66e102ac683822
CUNANAN-KYLE <kcunanan23-0259@cca.edu.ph>
2025-11-19 20:06:32 +0800

    changes (again)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facade\Auth;
+use Illuminate\Support\Facade\Hash;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    function showRegister(){
+        return view('authentication.register');
+    }
+
+     function showLogin(){
+        return view('authentication.login');
+    }
+
+    function register(Request $request){
+        $request->validate([
+            'name'      => 'required|string|max:255',
+            'email'     => 'required|email|unique:users',
+            'password'  => 'required|min:6|confirmed'
+        ]);
+        
+        User::create([
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'password'  => Hash::make($request->password)
+        ]);
+        return redirect()->route('login.form')->with('success', 'Registration successful');
+    }
+    function login(Request $request){
+
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('portfolio', 'portfolio');
+
+Route::get('/login',[AuthController::class, 'showLogin'])->name('login.form');
+Route::post('/login',[AuthController::class, 'performLogin'])->name('login');
+Route::get('/register',[AuthController::class, 'showRegister'])->name('register.form');
+Route::post('/register',[AuthController::class, 'performRegister'])->name('register');
+
+Route::post('/logout', function (){
+
+});
+
